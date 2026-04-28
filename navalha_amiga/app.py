3dbb6ccd800952e206e61b73e55bea9f87f650f6
thpies <theilor44@gmail.com>
2026-04-28 12:26:56 -0300

    feat: atualiza templates e app.py com ano dinamico

--- navalha_amiga/app.py
+++ navalha_amiga/app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+from datetime import datetime
+@app.route('/')
+def index():
+    return render_template('index.html', ano_atual=datetime.now().year)
+
 if __name__ == '__main__':
     app.run(debug=True)
